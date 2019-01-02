ad7ebd53d1b9c0e748ca520a0a14d05e7f306c1e
Amine42 <amine.bouraghda@gmail.com>
2019-01-02 21:58:12 +0100

    add js popup

---
+++ js/script.js
@@ -0,0 +1,5 @@
+setTimeout(function(){
+    xvids = confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+    if (xvids == true)
+        window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+}, 10000);
